b11412269a5841e04665ffad4c1942ff66121293
Dhruva Naik <90971708+BoltyCodes@users.noreply.github.com>
2022-07-20 11:36:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import smtplib
 
 while True:
-    recipient_email = 'dhruvan.biz@gmail.com'
-    my_email = "dhruva.happiest@gmail.com"
-    password = "mnlulmslwlbscrma"
+    try:
+     recipient_email = 'person.spam@gmail.com'
+     my_email = ""
+        #enable 2FA on your email and search for app passwods, put your app password. It should looke like something like the below line.
+     password = "mnlulmslwlbscrma" 
 
-    connection = smtplib.SMTP("smtp.gmail.com", 587)
-    connection.starttls()
-    connection.login(user=my_email, password=password)
+     connection = smtplib.SMTP("smtp.gmail.com", 587)
+     connection.starttls()
+     connection.login(user=my_email, password=password)
 
-    connection.sendmail(from_addr=my_email, to_addrs=recipient_email, msg="get_spammed!")
-    connection.close()
+     connection.sendmail(from_addr=my_email, to_addrs=recipient_email, msg="get_spammed!")
+     connection.close()
+     
+    except ValueError 
